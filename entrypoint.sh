51dde6964743039c3dc9831e133c31c46760260b
Martin Zdrahal <martin.zdrahal@s-team.at>
2017-07-17 10:06:49 +0200

    removed unnecessary configuration items from ceph client configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-printf "[global]\nfsid = $RBD_FSID\nmon_host = $RBD_MONITOR_IPS\nauth_cluster_required = cephx\nauth_service_required = cephx\nauth_client_required = cephx\n\nmon_allow_pool_delete = true\n" >/etc/ceph/ceph.conf
-printf "[client.$RBD_USER_NAME]\n        key = $RBD_SECRET\n" >/etc/ceph/ceph.client.$RBD_USER_NAME.keyring.conf
+printf "[global]\nmon_host = $RBD_MONITOR_IPS\n" >/etc/ceph/ceph.conf
+printf "[client.$RBD_USER_NAME]\n        key = $RBD_SECRET\n" >/etc/ceph/ceph.client.$RBD_USER_NAME.keyring
 
 if [ -z "$1" ]; then
   /bin/bash
